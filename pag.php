f176ec7da0b394728fc0b6d8f6c46eefc7407ed7
Samela <laniacbrito@gmail.com>
2019-11-12 21:43:48 -0300

    pagina

---
+++ pag.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>DOE VIDA</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/stylesheet1.css">
+		<link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<script type="text/javascript"  src="js/main.js"></script>
+		<script type="text/javascript"  src="jquery/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" defer src="Bootstrap/js/bootstrap.min.js"></script>
+		<link rel="icon" href="img/icon.png">
+	</head>
+
+	<body>
+		<div class="cover-container d-flex w-100 h-100 p-3  flex-column">
+			<header class="masthead mb-auto">
+				<div class="inner">
+					<img src="img/logo3.png">
+					<nav class="nav nav-masthead justify-content-center">
+						<a class="nav-link active" href="pag.html">Home</a>
+						<a class="nav-link" href="MaisInformações.html">Mais informações</a>
+						<a class="nav-link" href="imprimir.php">Mostrar Usuários</a>
+						<a class="nav-link" href="sair.php">Sair</a>
+					</nav>
+				</div>
+			</header>
+
+			<main>
+				<hr>
+				<div class="inicio">
+					<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+						<div class="carousel-inner">
+								<div class="carousel-item active">
+									<img class="d-block w-100" src="img/1.png"  height="590px" alt="First slide">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="img/2.png"  height="590px"  alt="Second slide">
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" src="img/3.png" height="590px"  alt="Third slide">
+								</div>
+						</div>
+							<a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+					</div>
+				</div>
+
+				<div id="slogan" style="background-image: url(img/fundo.gif)">
+					<h3> <b>Importante saber antes de doar sangue:</b></h3>
+				</div>
+				<section id="portifolio">
+					<div class="item-portifolio">
+							<h2> Sono </h2>
+							</br>
+							<div class="card border-info" style="width: 18rem;">
+									<img src="img/sono.jpg" class="card-img-top" height="213px">
+								<div class="card-body">
+									<p class="card-text" align="justify">Ter uma noite de sono de qualidade pode contribuir para uma doação de sucesso, pois evita que o paciente se sinta mal durante o processo.</p> <button type="button" class="btn btn-outline-danger">Saiba Mais</button>
+								</div>
+						</div> 
+					</div>
+					<div class="item-portifolio">
+						<h2> Alimentação </h2>
+								</br>
+								<div class="card border-success" style="width: 18rem;">
+									<img src="img/alimentação.jpg" class="card-img-top" >
+									<div class="card-body">
+										<p class="card-text" align="justify">Se a pessoa se alimentar de forma inadequada, um dia antes da doação, o sangue, doado na manhã seguinte, pode ser descartado.</p> <button type="button" class="btn btn-outline-danger">Saiba Mais</button>
+									</div>
+								</div>
+					</div>
+					<div class="item-portifolio">
+						<h2> Saúde </h2>
+								</br>
+							<div class="card border-primary" style="width: 18rem;">
+									<img src="img/saude.jpg" class="card-img-top">
+								<div class="card-body">
+									<p class="card-text" align="justify">A saúde é o fator mais importante e fundamental para a doação de sangue. Estar inato de qualquer doença é o mais importante a se verificar antes da doação.</p> <button type="button" class="btn btn-outline-danger">Saiba Mais</button>
+								</div>
+							</div>
+				</section>
+				<div id="slogan" style= "background-image: url(img/fundo.gif)">
+					<h3><b>Quem pode doar sangue?</b></h3>
+				</div>
+				<div class="card-group">
+						<div class="card">
+							<div class="card-body">
+								<p class="card-text"> 
+										<h5 class="card-title">REQUISITOS</h5>
+										<li> Ter entre 16 e 69 anos. Pessoas acima de 60 anos só podem doar se já tiverem doado sangue alguma vez antes dessa idade. </li> <br>
+										<li>Estar descansado (ter dormido pelo menos 6 horas nas últimas 24 horas) </li> <br>
+										<li>Apresentar documento original com foto, que permita o reconhecimento do candidato, emitido por órgão oficial (Carteira de Identidade, Cartão de Identidade de Profissional Liberal, Carteira de Trabalho e Previdência Social). </li> <br>
+										<li> Pesar no mínimo 50kg;</li> <br>
+										<li> Estar em boas condições de saúde</li>
+							</div>
+						</div>
+						<div class="card">
+							<img class="card-img-top" src="img/doação-sangue.png" alt="Imagem de capa do card">
+						</div>
+				</div>
+				<div id="slogan" style= "background-image: url(img/fundo.gif)">
+					<h3><b>Quem não pode doar sangue?</b></h3>
+				</div>
+				<div class="card-group">
+						<div class="card">
+								<img class="card-img-top" src="img/doe2.jpg" alt="Imagem de capa do card">
+						</div>
+						<div class="card">
+							<div class="card-body">
+								<p class="card-text"> 
+									<li> Estar com anemia no teste realizado imediatamente antes da doação </li> <br>
+									<li>Estar com hipertensão ou hipotensão arterial no momento da doação </li> <br>
+									<li>Estar com aumento ou diminuição dos batimentos cardíacos no momento da doação </li> <br>
+									<li> Estar com febre no dia da doação</li> <br></p>
+							</div>
+						</div>
+				</div>
+			</main>
+
+			<footer class="container py-5" class="container">
+					<div class="row">
+						<div class="col-4 col-md">
+							<h5>Sobre</h5>
+							<ul class="list-unstyled text-small">
+								<li><a class="text-muted" href="#">Equipe</a></li>
+								<li><a class="text-muted" href="#">Locais</a></li>
+								<li><a class="text-muted" href="#">Privacidade</a></li>
+								<li><a class="text-muted" href="#">Termos</a></li>
+							</ul>
+						</div>
+						<div class="col-6 col-md">
+						<h5>Doe Vida</h5>
+							<ul class="list-unstyled text-small">
+								<li><a class="text-muted" href="#">Projetos</a></li>
+								<li><a class="text-muted" href="#">Agendas</a></li>
+								<li><a class="text-muted" href="#">Parceria</a></li><li><a class="text-muted" href="#">Início</a></li>
+								<li><a class="text-muted" href="#">Investimentos</a></li>
+							</ul>
+						</div>
+						<div class="col-xs-12 col-sn-12 col-nd-4 col-lg-4"> 
+							<h5> Redes sociais: </h5> 
+							<img src="img/face.jpeg" height="20" width="20"> <a href="http://www.facebook.com" > doevida/facebook </a>
+							<br> 
+							<img src="img/insta.jpeg" height="20" width="20"> <a href="http://www.instagram.com"> @doe_vida </a>
+							<br> 
+							<img src="img/twitter.png" height="20" width="20"> <a href="https://twitter.com/login?lang=pt"> @doevida</a> 
+							<br>
+							<img src="img/zap.jpeg" height="20" width="20"> <a href="http://www.zap.com"> (77) 9 81364056 </a>    
+						</div>
+						<div class="col-xs-12 col-sn-12 col-nd-4 col-lg-4"> <p> &copy; copyright 2019 DoeVida <br> Gabriely Souza & Carine Macedo</p>
+						</div>
+					</div>
+			</footer>
+		</div>
+	</body>
+</html>
